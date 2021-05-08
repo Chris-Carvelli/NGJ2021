b0dbe424493cb342d79476d1c845d4a6accd3463
Chris <chris@modl.ai>
2021-05-08 17:16:30 +0200

    Inputs proto

---
+++ Assets/Src/Rhythm/MinigameController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+namespace Src.Rhythm {
+	public class MinigameController : MonoBehaviour {
+		private RhythmSystem _rhythmSystem;
+
+		public string[] inputNames = new[] {
+			"MinigameLeft",
+			"MinigameRight",
+			"MinigameUp",
+			"MinigameDown",
+		};
+		
+		public bool inputSnap;
+		public float tolerance = .2f;
+
+		public float[] lastInputPresses;
+		
+		private void Start() {
+			_rhythmSystem = RhythmSystem.Instance;
+
+			lastInputPresses = inputNames.Select(_ => 999f).ToArray();
+		}
+
+		private void Update() {
+			for (var i = 0; i < inputNames.Length; i++) {
+				var input = inputNames[i];
+				if (Input.GetButtonDown(input)) {
+					var rawPress = _rhythmSystem.Time;
+					var snappedTime = Mathf.Round(rawPress);
+					var dSnap = Mathf.Abs(rawPress - snappedTime);
+
+
+					lastInputPresses[i] = dSnap < tolerance ? snappedTime : rawPress;
+				}
+			}
+		}
+	}
+}
